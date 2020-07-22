a3ae822763dbc9f738b0f7b239a5100037bc3a93
Danucas <danrodcastillo1994@gmail.com>
2020-07-21 22:45:18 -0500

    librosa sounddevice imported for python

--- install_modules.sh
+++ install_modules.sh
@@ -1,6 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
+# install pip3 and python required dependencies for
+# Audio processing
 sudo python3 -m pip install librosa tk sounddevice soundfile wave
+
+# Install Audio codecs and libraries
 sudo apt-get install ffmpeg
 sudo apt-get install libportaudio2
 sudo apt-get install libasound-dev
