5cf5f6cd18dc8ff12d9a46ce7a4387e34c7f9f59
Danucas <danrodcastillo1994@gmail.com>
2020-07-21 23:16:54 -0500

    entry point

--- rosan.py
+++ rosan.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 """
-run graphic analisis
+Entry point for the Spectrum Analyser
 """
 
 from spectrum_analyser import Window
 from sound_reader import AudioFile
 
-
+# Initializes the window and the main loop
 win = Window()
-
 win.root.mainloop()
