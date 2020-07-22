59acabceed640e0b13d86c7ca77d637ea4d74111
Danucas <danrodcastillo1994@gmail.com>
2020-07-21 22:49:39 -0500

    build an APK for record Audio on Android

--- kivy.sh
+++ kivy.sh
@@ -1,14 +1,23 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-#install buildozer
+# Download buildozer source
 git clone https://github.com/kivy/buildozer.git
 cd buildozer
+# Compile and install from source
 sudo python setup.py
 cd ..
+# Remove the cloned repo
 rm -rf buildozer
 
+# Update and install JDK
 sudo apt update
-sudo apt install -y git zip unzip openjdk-8-jdk python3-pip autoconf libtool pkg-config zlib1g-dev libncurses5-dev libncursesw5-dev libtinfo5 cmake libffi-dev
+sudo apt install -y git zip unzip \
+    openjdk-8-jdk python3-pip autoconf \
+    libtool pkg-config zlib1g-dev \
+    libncurses5-dev libncursesw5-dev \
+    libtinfo5 cmake libffi-dev
+
+# Python dependencies for buildozer build
 pip3 install --user --upgrade cython virtualenv
 
 sudo apt-get install cython
